894f0c6a96b868feed62979a3cb6b4509cfd3da0
tarunkhurana <khurana.tarun.tk@gmail.com>
2017-09-11 01:38:50 +0530

    es6 commits

---
+++ learning/es6/udemy/essential in javascript/webpack.config.js
@@ -0,0 +1,15 @@
+var path = require("path");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            use: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+}
