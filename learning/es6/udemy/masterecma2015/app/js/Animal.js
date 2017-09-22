11ea7c640dac8e83c928400eece92a0ea7a0f6dc
Tarun Khurana <Tarun.Khurana@rsystems.com>
2017-09-22 23:30:10 +0530

    Removed folder from repository

---
+++ learning/es6/udemy/masterecma2015/app/js/Animal.js
@@ -0,0 +1,9 @@
+export const CLASSNAME="Animal";
+export default class Animal{
+    constructor(){
+        console.log("This is animal class")
+    }
+    getClassType(){
+        return "Animal";
+    }
+}
---
+++ learning/es6/udemy/masterecma2015/app/js/Math.js
@@ -0,0 +1,2 @@
+export const add=(n1,n2)=>n1+n2;
+export const subtract=(n1,n2)=>n1-n2;
